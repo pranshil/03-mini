0cd4ee805f04f633bb43f92febb997ad856a6ca1
Pranshil <pranshil94@gmail.com>
2022-04-22 23:57:45 +0530

    make it better

--- src/main/java/com/tcs/controller/PlanController.java
+++ src/main/java/com/tcs/controller/PlanController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.tcs.entity.Plans;
 import com.tcs.service.PlanService;
 
 @RestController
+@CrossOrigin
 public class PlanController {
 
 	@Autowired
